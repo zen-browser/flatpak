ffa9590177c4eeac65d742a66e02eb11de5e1ba4
Mauro Balades <mauro.balades@tutanota.com>
2024-07-27 15:14:54 +0200

    chore: Update Zen Browser configuration files, icons, and metainfo

--- launch-script.sh
+++ launch-script.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 export TMPDIR=$XDG_CACHE_HOME/tmp
 
-MOZ_LAUNCHER="$MOZ_DIST_BIN/launch-script.sh"
-##
-## Enable Wayland backend?
-##
-if ! [ $MOZ_DISABLE_WAYLAND ] && [ "$WAYLAND_DISPLAY" ]; then
-  if [ "$XDG_CURRENT_DESKTOP" == "GNOME" ]; then
-    export MOZ_ENABLE_WAYLAND=1
-  fi
-##  Enable Wayland on KDE/Sway
-##
-  if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
-    export MOZ_ENABLE_WAYLAND=1
-  fi
-fi
-
-exec /app/bin/zen "$@"
+exec /app/zen/zen "$@"
